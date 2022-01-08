4760053b92281baf0703418c177b9de41d32a8fb
Aakarshit1999 <aakarshit6143@gmail.com>
2022-01-08 13:54:33 +0530

    users

--- frontend/src/user/user.js
+++ frontend/src/user/user.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
+import {list} from "./apiuser"
+
 class Users extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            Users: []
+        }
+    }
+  
+    componentDidMount() {
+        list().then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                this.setState({Users: data})
+            }
+        })
+    }
+
     render() { 
         return(
             <div className='container'>
