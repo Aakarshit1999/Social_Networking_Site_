1a09bfe3032c217b32177a6f3c29aa84a70bbd70
Aakarshit1999 <aakarshit6143@gmail.com>
2022-01-09 17:56:30 +0530

    users page updated

--- frontend/src/user/user.js
+++ frontend/src/user/user.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {list} from "./apiuser"
-
+import {list} from "./apiuser";
+import avatar from "../images/avatar.jpg";
+import {Link} from 'react-router-dom';
 class Users extends Component {
 
     constructor() {
@@ -20,10 +21,33 @@ class Users extends Component {
         })
     }
 
+
+    renderusers = Users => (
+        <div className='row'>
+                    {Users.map((user, i) => 
+                        <div className="card mr-2" style={{width: '18rem'}} key={i}>
+                        <img src={avatar} 
+                        className="card-img-top" 
+                        alt={user.name}
+                        style={{width: '100%' , height: '15vw', objectFit: 'cover'}}
+                        />
+                        <div className="card-body">
+                          <h5 className="card-title">{user.name}</h5>
+                          <p className="card-text">{user.email}</p>
+                          <Link to={`/user/${user._id}`} className="btn btn-raised btn-primary btn-sm">View Profile</Link>
+                        </div>
+                      </div>
+                    )}
+                </div>
+    );
+
     render() { 
+        const {Users} = this.state;
         return(
             <div className='container'>
                 <h2 className='mt-5 mb-5'>Users</h2>
+
+               { this.renderusers(Users)}
             </div>
         )
     }
