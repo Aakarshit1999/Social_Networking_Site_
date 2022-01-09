946135660b943748ec9a3ca5c54b9ef310df2abb
Aakarshit1999 <aakarshit6143@gmail.com>
2022-01-09 17:55:51 +0530

    profiling updated

--- frontend/src/user/profile.js
+++ frontend/src/user/profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from "../auth/index"
 import { Redirect, Link } from "react-router-dom"
-import { read } from "./apiuser"
+import { read } from "./apiuser";
+import avatar from "../images/avatar.jpg";
+import Deleteuser from './deleteuser';
 
 class Profile extends Component {
     constructor() {
@@ -32,34 +34,45 @@ class Profile extends Component {
         this.init(userId);
     }
 
+    componentWillReceiveProps(props) {
+        const userId = props.match.params.userId;
+        this.init(userId);
+    }
+
     render() {
         const { redirectToSignin, user } = this.state;
         if (redirectToSignin) return <Redirect to="/signin" />;
 
         return (
             <div className='container'>
+                <h2 className='mt-5 mb-5'>Profile</h2>
                 <div className='row'>
                     <div className='col-md-6'>
-                        <h2 className='mt-5 mb-5'>Profile</h2>
-                        <p>Hello {isAuthenticated().user.name}</p>
-                        <p>Email: {isAuthenticated().user.email}</p>
+                        
+                        <img src={avatar} 
+                        className="card-img-top" 
+                        alt={user.name}
+                        style={{width: '100%' , height: '15vw', objectFit: 'cover'}}
+                        />
+                    </div>
+                    <div className='col-md-6'>
+                        <div className='lead'>
+                            <p>Hello {user.name}</p>
+                        <p>Email: {user.email}</p>
                         <p>{`Joined ${new Date(
                             user.created
                         ).toDateString()}`}</p>
-                    </div>
-                    <div className='col-md-6'>
+                        </div>
                         {isAuthenticated().user &&
-                            isAuthenticated().user._id == user._id && (
-                                <div className='d-inline-block mt-5'>
+                            isAuthenticated().user._id === user._id && (
+                                <div className='d-inline-block'>
                                     <Link
                                         className='btn btn-raised btn-success mr-5'
                                         to={`/user/edit/${user._id}`}
                                     >
                                         Edit Profile
                                     </Link>
-                                    <button className='btn btn-raised btn-danger '>
-                                        Delete Profile
-                                    </button>
+                                    <Deleteuser />
                                 </div>
                             )}
                     </div>
